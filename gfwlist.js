9481628a1f4c3be14d404c994f48b7ff0fdd7937
deju <deju.tu@gmail.com>
2018-11-22 10:51:37 +0800

    feat(list): list update

--- gfwlist.js
+++ gfwlist.js
@@ -4,6 +4,9 @@
 var proxy = "SOCKS5 127.0.0.1:1080; SOCKS 127.0.0.1:1080; DIRECT;";
 
 var rules = [
+  "||termius.com",
+  "||hbg.com",
+  "||coin.dance",
   "||t.co",
   "||bwbx.io",
   "||dribbble.com",
